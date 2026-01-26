e4935f0b1a4e2b7660fc2e82638b77c45ce917b2
Fernando Gasperi <fgasperijabalera@gmail.com>
2026-01-26 18:00:19 -0300

    Set up project with Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(11)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}

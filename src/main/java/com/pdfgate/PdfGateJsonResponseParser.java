bcd4a2d3b0bd6c02ca23d879652d092257d2ace7
Fernando Gasperi <fgasperijabalera@gmail.com>
2026-01-29 12:48:49 -0300

    Remove unused imports

--- src/main/java/com/pdfgate/PdfGateJsonResponseParser.java
+++ src/main/java/com/pdfgate/PdfGateJsonResponseParser.java
@@ -1,9 +1,6 @@
 package com.pdfgate;
 
-import okhttp3.Call;
-import okhttp3.Response;
 import okhttp3.ResponseBody;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -15,8 +12,7 @@ public class PdfGateJsonResponseParser extends PdfGateResponseParser {
 
     @Override public PdfGateDocument parseResponse(ResponseBody responseBody) throws IOException {
         String responseJson = responseBody == null ? "" : responseBody.string();
-        PdfGateDocument document = PdfGateJson.gson().fromJson(responseJson, PdfGateDocument.class);
 
-        return document;
+        return PdfGateJson.gson().fromJson(responseJson, PdfGateDocument.class);
     }
 }
